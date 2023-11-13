73a4485e5e399155f28fde764ec6357b1d307f1b
miyakeraito <dr.surannparare@gmail.com>
2023-11-13 11:38:34 +0900

    aa

--- 尻取り/HTML/検証.js
+++ 尻取り/HTML/検証.js
@@ -1,5 +1,4 @@
 function aaaa() {
     document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 0xffffff).toString(16).padStart(6,"0");
-    requestAnimationFrame(aaaa);
 }
-aaaa();
+setInterval(aaaa,500);
