2255c0d8d0c7d4d9c0ccc347c52a233bc427abae
miyakeraito <dr.surannparare@gmail.com>
2023-12-04 10:19:44 +0900

    a

---
+++ 尻取り/HTML/検証のコピー.js
@@ -0,0 +1,5 @@
+$("#particle").particleText({
+    text: "尻取り", // 表示させたいテキスト。改行の場合は<br>追加
+    colors:["#30D5C8","#30D5C8", "#30D5C8"], // パーティクルの色を複数指定可能
+    speed: "slow",  // slow, middle, high の3つから粒子が集まる速さを選択
+  });
